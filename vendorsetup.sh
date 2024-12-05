c4df18cc81e62a3a3f7dd1bb9737a562e2b713f0
luuke <zkluuke@gmail.com>
2024-12-05 19:29:21 +0900

    Update vendorsetup.sh to OrangeFox

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,80 @@
 #
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2021-2022 The OrangeFox Recovery Project
 #
-# SPDX-License-Identifier: Apache-2.0
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
 #
 
-add_lunch_combo twrp_lyriq-user
-add_lunch_combo twrp_lyriq-userdebug
-add_lunch_combo twrp_lyriq-eng
+FDEVICE="lyriq"
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w \"$FDEVICE\")
+	if [ -n "$chkdev" ]; then 
+		FOX_BUILD_DEVICE="$FDEVICE"
+	else
+		chkdev=$(set | grep BASH_ARGV | grep -w \"$FDEVICE\")
+		[ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+	fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+	echo "** WARNING **: Always set FOX_BUILD_DEVICE to the device codename before starting to build for any device!"
+	fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+	# Building
+	export LC_ALL="C"
+ 	export ALLOW_MISSING_DEPENDENCIES=true
+	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+
+	# General settings
+	export FOX_VERSION=12.1
+	export OF_MAINTAINER="Luke"
+	export TARGET_DEVICE_ALT="lyriq,XT2303-1,XT2303-2,XT2303-3"
+	export FOX_TARGET_DEVICES="lyriq,XT2303-1,XT2303-2,XT2303-3"
+	export OF_AB_DEVICE_WITH_RECOVERY_PARTITION=0
+	export FOX_AB_DEVICE=1
+	export FOX_VANILLA_BUILD=1
+	export OF_DISABLE_OTA_MENU=1
+	export OF_USE_LZ4_COMPRESSION=1
+	export OF_USE_GREEN_LED=0
+        export OF_QUICK_BACKUP_LIST="/boot;/data;"
+	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+	export FOX_ENABLE_APP_MANAGER=1
+
+	# Binaries
+	export FOX_USE_TAR_BINARY=1
+	export FOX_USE_SED_BINARY=1
+	export FOX_USE_XZ_UTILS=1
+	export OF_ENABLE_LPTOOLS=1
+
+	# Screen settings
+	export OF_SCREEN_H=2400
+	export OF_STATUS_H=74
+	export OF_STATUS_INDENT_LEFT=48
+	export OF_STATUS_INDENT_RIGHT=48
+	export OF_CLOCK_POS=1
+	export OF_HIDE_NOTCH=1
+
+	# Let's see what are our build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+		export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+		export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+		export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+		export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+	fi
+fi
