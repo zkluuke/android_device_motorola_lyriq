9260ac4f94a59f7b7c68b816ef4063781ee6a6aa
luuke <zkluuke@gmail.com>
2024-12-05 18:46:52 +0900

    Update omni_lyriq.mk

--- omni_lyriq.mk
+++ omni_lyriq.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from lyriq device
 $(call inherit-product, device/motorola/lyriq/device.mk)
 
 PRODUCT_DEVICE := lyriq
-PRODUCT_NAME := omni_lyriq
+PRODUCT_NAME := twrp_lyriq
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := motorola edge 40
 PRODUCT_MANUFACTURER := motorola
